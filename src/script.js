d76af21255ed64b0431378f436e4d63ff071779c
Boluwatife <jaycewap@gmail.com>
2021-07-16 21:41:42 +0100

    Final works

--- src/script.js
+++ src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+//  Loader
+const textureLoader = new THREE.TextureLoader()
+const normalLoader = textureLoader.load('/NormalMap.png')
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,12 +17,14 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
+const geometry =  new THREE.SphereBufferGeometry(.8, 64, 64)
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial()
+material.roughness = .2
+material.metalness = .7
+material.color = new THREE.Color(0x292929)
+material.normalMap = normalLoader;
 
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
@@ -26,11 +32,25 @@ scene.add(sphere)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.PointLight(0xffffff, .3)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
+
+const pointLight2 = new THREE.PointLight(0xff000, .1)
+pointLight2.position.x = -2
+pointLight2.position.y = -2
+pointLight2.position.z = 4
+scene.add(pointLight2)
+
+gui.add(pointLight2.position, 'y').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'x').min(-3).max(3).step(0.01)
+gui.add(pointLight2.position, 'z').min(-6).max(6).step(0.01)
+gui.add(pointLight2, 'intensity').min(0).max(20).step(1)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1)
+scene.add(pointLightHelper)
 
 /**
  * Sizes
@@ -73,7 +93,8 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
